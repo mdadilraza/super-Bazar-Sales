710b7684a9870c8b398a70ea4571a35cdea2f609
MD ADIL RAZA <95642943+mdadilraza@users.noreply.github.com>
2025-02-07 14:30:40 +0530

    Update SalesReports.java

--- src/main/java/com/eidiko/super_bazar_sales/dto/SalesReports.java
+++ src/main/java/com/eidiko/super_bazar_sales/dto/SalesReports.java
@@ -3,10 +3,12 @@ package com.eidiko.super_bazar_sales.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Builder;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class SalesReports {
     private String productName;
     private int sale ;
